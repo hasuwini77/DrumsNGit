b4aa218940ce9fd8136ed56419c6f2d8bd8fdafe
Edwin Andermyr <andermyredwin@gmail.com>
2024-02-06 13:09:41 +0100

    darkmode switch button working fine

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ const darkButton = document.querySelector(".dark-button");
 
 darkButton.addEventListener("click", () => {
   document.querySelector("body").classList.toggle("dark-mode");
+
+  let darkModeElem = document.querySelector(".dark-mode");
+  if (darkModeElem) {
+    darkButton.innerHTML = `
+    <img class="light-icon" src="./img/mode.png" alt="light-mode-icon" />`;
+  } else {
+    darkButton.innerHTML = ` 
+        <img class="dark-icon" src="./img/night-mode.png" alt="dark-mode-icon" /> `;
+  }
 });
