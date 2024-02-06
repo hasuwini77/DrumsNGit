960776bb56369bc513abf220c32ba77dd314b0b6
Edwin Andermyr <andermyredwin@gmail.com>
2024-02-06 19:09:28 +0100

    Cookie fully working on localhost

--- index.js
+++ index.js
@@ -83,10 +83,8 @@ const executeCodes = () => {
     button.addEventListener("click", () => {
       cookieBox.classList.remove("show");
 
-      //if button has acceptBtn id
-      if (button.id == "acceptBtn") {
-        //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
-        document.cookie = "cookieBy= DrumsNGit; max-age=" + 60 * 60 * 24 * 30;
+      if (button.id === "acceptBtn") {
+        document.cookie = "cookieBy=DrumsNGit; max-age=" + 60 * 60 * 24 * 30 + "; path=/";
       }
     });
   });
